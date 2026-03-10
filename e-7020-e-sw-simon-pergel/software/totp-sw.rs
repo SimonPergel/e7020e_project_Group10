b033697f04a93a9a145fdc00f2e8871363d19edc
Simon Pergel <isoepe-1@student.ltu.se>
2026-03-10 16:50:34 +0100

    started with the generated opt functions

--- e-7020-e-sw-simon-pergel/software/totp-sw.rs
+++ e-7020-e-sw-simon-pergel/software/totp-sw.rs
@@ -1,6 +1,4 @@
-#[task(binds = USBD, priority = 1)]
-    fn OTP(mut cx: usb_interrupt::Context) {
-        // Convert time, both the server and the PCB devices
+// Convert time, both the server and the PCB devices
                 // - TOTP does not use raw time
                 // - time is devided into fixed time windows(30 sec)
                 // - this makes shore the OTP stayes the same for the whole window
@@ -9,6 +7,49 @@
                 // - Standard is 8 bytes counter to ensure consistant hashing across system
         // Compute(generate) the hash - secret + counte
         // Choose the starting posistion in the hash
-        // convert to OTP
+                // the last bytes of the hash determines where to extract the code
+        // convert to OTP to 6 digit code
         // 
+// Parameters that must match the server
+
+// 1. Secret key: shared key(usually Base32)
+// 2. Hash algo.: SHA1 (most common)
+// 3. Time step: should be 30 sec
+// 4. Digit of the code: typically 6 digits(sometimes 8)
+// 5. Current time: UNIX time
+
+// Usfull doc: https://docs.rs/otp-auth/latest/otp/struct.Totp.html
+impl Totp
+
+pub fn new(
+        alg: Algoritm,
+        issuer: String,
+        label: String,
+        digits: u8,
+        period: u64,
+        secret: Secret,
+) -> Self
+
+use otp:: {Totp, Algoritm, Secret};
+// Create a totp insatance
+let totp = Totp:: new(
+        Algoritm::SHA1,
+        "MyDevice".into(),
+        "user1".into(),
+        6,                                      // number of digits
+        30,                                     // period(time step)
+        Secret::from_bytes(b"supersecret")      // secret key
+)
+
+#[task(binds = USBD, priority = 1)]
+    fn gen_OTP(mut cx: usb_interrupt::Context) {
+
+        // get current time for rtc(real time clock)
+        let unix_time: u64 = rtc_get_unic_time();
+
+        // generate OPT
+        let opt = totp.generate_at(unix_time);
+
+         
+        
     }
